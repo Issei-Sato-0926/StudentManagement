b81d5de478948c0467ce04d90820117687a36ef4
Issei-Sato-0926 <satoon.0926@outlook.jp>
2025-05-09 17:09:00 +0900

    27_Thymeleafを使ったReadの画面描画処理

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,14 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.controller.converter.StudentConverter;
 import com.example.demo.data.Student;
 import com.example.demo.data.StudentCourses;
-import com.example.demo.domain.StudentDetail;
 import com.example.demo.service.StudentService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -23,11 +23,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.getStudentCourseList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentCourseList")
