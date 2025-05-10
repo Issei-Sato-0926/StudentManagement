32178d1f6fba27a1b989280141a386ed5b9da89b
Issei-Sato-0926 <satoon.0926@outlook.jp>
2025-05-10 13:08:43 +0900

    29_受講生情報登録処理の実装

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -2,10 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.data.Student;
 import com.example.demo.data.StudentCourses;
+import com.example.demo.domain.StudentDetail;
 import com.example.demo.repository.StudentRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -30,5 +33,17 @@ public class StudentService {
 
   public List<StudentCourses> getStudentCourseList() {
     return repository.searchStudentCourses();
+  }
+
+  @Transactional
+  public void registerStudent(StudentDetail studentDetail) {
+    repository.registerStudent(studentDetail.getStudent());
+    // TODO: コース情報登録も行う。
+    for (StudentCourses studentCourses : studentDetail.getStudentCourses()) {
+      studentCourses.setStudentId(studentDetail.getStudent().getId());
+      studentCourses.setCourseStartAt(LocalDateTime.now());
+      studentCourses.setCourseEndAt(LocalDateTime.now().plusYears(1));
+      repository.registerStudentCourses(studentCourses);
+    }
   }
 }
