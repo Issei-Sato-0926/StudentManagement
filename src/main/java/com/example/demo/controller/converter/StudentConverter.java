e6b23ab4372937d3b14ce6196442c02302f6510f
Issei-Sato-0926 <satoon.0926@outlook.jp>
2025-05-13 15:53:23 +0900

    34_Create処理のREST化とリファクタリング

--- src/main/java/com/example/demo/controller/converter/StudentConverter.java
+++ src/main/java/com/example/demo/controller/converter/StudentConverter.java
@@ -8,9 +8,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
+/**
+ * 受講生詳細を受講生や受講生コース情報、もしくはその逆を行うコンバーターです。
+ */
 @Component
 public class StudentConverter {
 
+  /**
+   * 受講生に紐づく受講生コース情報をマッピングする。 受講生コース情報は受講生に対して複数存在するのでループを回して受講生詳細情報を組み立てる。
+   *
+   * @param students       受講生一覧
+   * @param studentCourses 受講生コース情報のリスト
+   * @return 受講生詳細情報のリスト
+   */
   public List<StudentDetail> convertStudentDetails(List<Student> students,
       List<StudentCourses> studentCourses) {
     List<StudentDetail> studentDetails = new ArrayList<>();
--- src/main/java/com/example/demo/domain/StudentDetail.java
+++ src/main/java/com/example/demo/domain/StudentDetail.java
@@ -3,11 +3,15 @@ package com.example.demo.domain;
 import com.example.demo.data.Student;
 import com.example.demo.data.StudentCourses;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDetail {
 
   private Student student;
